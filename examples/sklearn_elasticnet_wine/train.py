99baf1ff1c0c1a4d25b5464f82dee64b93e71ec0
victorviro <victor_sanchez91@hotmail.es>
2020-09-30 17:25:11 +0200

    Refactorization: Downloading data from website (removed dataset), using click for command line interfaces. Removed train notebook.

--- examples/sklearn_elasticnet_wine/train.py
+++ examples/sklearn_elasticnet_wine/train.py
@@ -1,8 +1,8 @@
 # The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
 
-import os
+
 import warnings
-import sys
+import click
 
 import pandas as pd
 import numpy as np
@@ -15,11 +15,11 @@ from sklearn.linear_model import ElasticNet
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 import mlflow
 import mlflow.sklearn
 
 #mlflow.set_tracking_uri('file:/home/viro/mlrun_store')
+
 
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
@@ -29,13 +29,17 @@ def eval_metrics(actual, pred):
 
 
 
-if __name__ == "__main__":
+@click.command()
+@click.option("--alpha", type=click.FLOAT, default=0.1, help="Constant that multiplies the penalty terms.")
+@click.option("--l1_ratio", type=click.FLOAT, default=0.1, help="The ElasticNet mixing parameter.")
+@click.argument("training_data", default="http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv")
+
+def run(training_data, alpha, l1_ratio):
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
-    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
-    wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
-    data = pd.read_csv(wine_path)
+    # Read the wine-quality csv file
+    data = pd.read_csv(training_data, sep=";")
 
     # The predicted column is "quality" which is a scalar from [3, 9]
     X = data.drop(["quality"], axis=1)
@@ -48,11 +52,8 @@ if __name__ == "__main__":
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
-
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
-    with mlflow.start_run(): #experiment_id=1
+    
+    with mlflow.start_run(experiment_id=0): #experiment_id=1
         model = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         
         fit_elasticnet = model.fit(X_train_scaled, y_train)
@@ -64,7 +65,7 @@ if __name__ == "__main__":
         print(f'Elasticnet model (alpha={alpha}, l1_ratio={l1_ratio}):')
         print(f'  RMSE: {rmse}\n  MAE: {mae}\n  R2: {r2}')
 
-        #we use the MLflow tracking APIs to log information about each training run
+        # use the MLflow tracking APIs to log information about each training run
         # hyperparameters
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
@@ -73,7 +74,7 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        # serialize the model in a format that MLflow knows how to deploy
+        # serialize the model in a format that MLflow knows how to deploy it
         mlflow.sklearn.log_model(model, "model")
 
 
@@ -92,6 +93,7 @@ if __name__ == "__main__":
         # Log artifacts (output files)
         mlflow.log_artifact("Importance_features.png")
 
-
+if __name__ == "__main__":
+    run()
 
         
